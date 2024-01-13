7a84714000e58d65deee14a456cf0d31c47dabe2
Shtein377 <beren-84@mail.ru>
2024-01-13 14:34:59 +0300

    add template

--- blog-app/routes/web.php
+++ blog-app/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+/*Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
+Route::get('/', [HomeController::class, 'homepage']);
+
+Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
+
+/*Route::get('post', [HomeController::class, 'post'])->middleware(['auth','admin']);*/
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
